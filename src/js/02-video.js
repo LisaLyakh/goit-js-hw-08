ae2151f802471d93fee37208a501be7391f66cc1
Lisa_Lyakh <99537140+LisaLyakh@users.noreply.github.com>
2022-09-03 10:42:18 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,7 @@ import { Player } from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Vimeo.Player(iframe); 
 
 const onPlayTime = function (data) {
     localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
